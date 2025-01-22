50f9d63867544b44efd0f055c358bad1b3172bcb
168742464+Sachin-A-Purkar@users.noreply.github.com <168742464+Sachin-A-Purkar@users.noreply.github.com>
2025-01-22 12:55:55 +0530

    update 2

--- doctor/src/Landing/Header.jsx
+++ doctor/src/Landing/Header.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
 import "./Header.css"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowRight } from '@fortawesome/free-solid-svg-icons';
+
 
 import H1 from "../images/heart.png"
+
 
 export default function Header() {
   return (
@@ -11,13 +15,21 @@ export default function Header() {
       </div>
       <div className='headbar'>
             <div className='part1'>
-                <img src={H1} alt='logo' className='H1'/><br/>
-                <h2>Apex Hospital</h2>
+                <img src={H1} alt='logo' className='himg1'/>
+                <h2 className='text1'>Apex Hospital</h2>
             </div>
             <div className='part2'>
-                
+                <a href="#home"> HOME </a>
+                <a href="#home"> ABOUT </a>
+                <a href="#home"> SERVICES </a>
+                <a href="#home"> PAGES </a>
+                <a href="#home"> CONTACT </a>
             </div>
-           
+            <a href='#home'>
+              <div className="part3">
+                Appointment <FontAwesomeIcon icon={faArrowRight} />
+              </div>
+            </a>
             
       </div>
     </div>
