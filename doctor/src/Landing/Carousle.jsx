268d976ec6276145525ef18f0b233bb8a0ecc496
168742464+Sachin-A-Purkar@users.noreply.github.com <168742464+Sachin-A-Purkar@users.noreply.github.com>
2025-01-24 11:00:01 +0530

    changes

--- doctor/src/Landing/Carousle.jsx
+++ doctor/src/Landing/Carousle.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import CountUp from 'react-countup';
 import './Carousle.css'; // CSS for the carousel
 import image1 from '../images/carousel-1.jpg';
 import image2 from '../images/carousel-2.jpg';
@@ -16,20 +17,41 @@ const Carousle = () => {
   };
 
   return (
-
+    <div className='carbody'>
+      <div className='cbox1'>
+        <div className='totalt'><h1 className='chead1'>" Good Health is the Foundation of<br/> a <u>Happy Life</u>"</h1></div>
+        <div className='total'>
+          <div className='total1'>
+            <h2 >Total Patients</h2>
+            <h1><CountUp start={1000} end={12678} duration={2} /></h1>
+          </div>
+          <div className='total2'>
+            <h2 >Total Doctors</h2>
+            <h1><CountUp start={10} end={124} duration={2} /></h1>
+          </div>
+          <div className='total3'>
+            <h2>Total Staff</h2>
+            <h1><CountUp start={10} end={678} duration={2} /></h1>
+          </div>
+        </div>
+      </div>
+      <div className='cbox2'>
         <div className="carousel-container">
-        <div className="carousel">
-            <div className="carousel-item">
-            <img src={images[currentIndex]} alt={`Slide ${currentIndex + 1}`} />
-            </div>
-        </div>
-        <button className="prev-btn" onClick={() => moveSlide(-1)}>
-            &#10094;
-        </button>
-        <button className="next-btn" onClick={() => moveSlide(1)}>
-            &#10095;
-        </button>
-        </div>
+          <div className="carousel">
+              <div className="carousel-item">
+              <img src={images[currentIndex]} alt={`Slide ${currentIndex + 1}`} />
+              </div>
+          </div>
+          <button className="prev-btn" onClick={() => moveSlide(-1)}>
+              &#10094;
+          </button>
+          <button className="next-btn" onClick={() => moveSlide(1)}>
+              &#10095;
+          </button>
+          </div>
+      </div>  
+    </div>
+        
   );
 };
 
